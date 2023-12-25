7b0004e0693c358f2cbd102cd786a5ee5bde5f9a
tenpadrummer <yoshiro.kato@kate-lier.com>
2023-12-25 22:48:49 +0900

    :fire: チャットルーム一覧の実装

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,11 @@
 <template>
   <div id="app">
-    <router-view />
-    <ApiTest />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ApiTest from './components/ApiTest.vue'
-
-export default {
-  name: 'App',
-  components: {
-    ApiTest
-  }
-}
+  export default {
+    name: 'App',
+  };
 </script>
