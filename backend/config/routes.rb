9e0eda2c76555dbeb6007fdf7ec46fd4a61d1f13
tenpadrummer <yoshiro.kato@kate-lier.com>
2023-12-25 22:44:33 +0900

    :fire: メッセージ管理APIの実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # 接続テスト
   get '/api/test', to: 'application#test'
 
-  resources :rooms, only: [:index, :create]
+  resources :rooms, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
